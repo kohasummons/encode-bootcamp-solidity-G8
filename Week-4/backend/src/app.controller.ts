4f8bfb502821528d2200d8cd4564365505753746
msv-654 <654mstv@gmail.com>
2024-04-06 22:42:37 +0200

    back logic implementing

--- Week-4/backend/src/app.controller.ts
+++ Week-4/backend/src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { MintTokenDto } from './dtos/mintToken.dto';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('contract-address')
+  getContractAddress() {
+    return { result: this.appService.getContractAddress() };
+  }
+
+  @Get('token-name')
+  async getTokenName() {
+    return { result: await this.appService.getTokenName() };
+  }
+
+  @Get('total-supply')
+  async getTotalSupply() {
+    return { result: await this.appService.getTotalSupply() };
+  }
+
+  @Get('token-balance/:address')
+  async getTokenBalance(@Param('address') address: string) {
+    return { result: await this.appService.getTokenBalance(address) };
+  }
+
+  @Get('transaction-receipt')
+  async getTransactionReceipt(@Query('hash') hash: string) {
+    return { result: await this.appService.getTransactionReceipt(hash) };
+  }
+
+  @Get('server-wallet-address')
+  getServerWalletAddress() {
+    return { result: this.appService.getServerWalletAddress() };
+  }
+
+  @Get('check-minter-role')
+  checkMinterRole(@Query('address') address: string) {
+    return { result: this.appService.checkMinterRole(address) };
+  }
+
+  @Post('mint-tokens')
+  async mintTokens(@Body() body: MintTokenDto) {
+    return { result: this.appService.mintTokens(body.address) };
   }
 }
--- Week-4/backend/src/app.service.ts
+++ Week-4/backend/src/app.service.ts
@@ -1,8 +1,87 @@
+import * as tokenJson from './assets/MyToken.json';
 import { Injectable } from '@nestjs/common';
+import * as chains from 'viem/chains';
+import * as process from 'process';
+import {
+  createPublicClient,
+  createWalletClient,
+  formatEther,
+  http,
+} from 'viem';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  publicClient;
+  walletClient;
+
+  constructor() {
+    const rpcEndpointUrl = process.env.RPC_ENDPOINT_URL;
+    const transport = http(rpcEndpointUrl);
+
+    this.publicClient = createPublicClient({
+      chain: chains.sepolia,
+      transport: transport,
+    });
+    this.walletClient = createWalletClient({
+      chain: chains.sepolia,
+      transport: http(process.env.PRIVATE_KEY),
+    });
+  }
+
+  async getTokenName(): Promise<any> {
+    return await this.publicClient.readContract({
+      address: this.getContractAddress() as `0x${string}`,
+      abi: tokenJson.abi,
+      functionName: 'name',
+    });
+  }
+
+  getContractAddress(): string {
+    return '0x7a9cf487143827659caE112e55f08a2F6975e058';
+  }
+
+  async getTotalSupply() {
+    const totalSupply = await this.publicClient.readContract({
+      address: this.getContractAddress() as `0x${string}`,
+      abi: tokenJson.abi,
+      functionName: 'totalSupply',
+    });
+    return formatEther(totalSupply as bigint);
+  }
+
+  async getTokenBalance(address: string) {
+    const totalSupply = await this.publicClient.readContract({
+      address: this.getContractAddress() as `0x${string}`,
+      abi: tokenJson.abi,
+      functionName: 'balanceOf',
+      args: [address],
+    });
+    return formatEther(totalSupply as bigint);
+  }
+
+  getTransactionReceipt(hash: string) {
+    const transactionReceipt = this.publicClient.getTransactionReceipt({
+      hash: hash,
+    });
+    console.log({ transactionReceipt });
+    transactionReceipt.blockNumber = transactionReceipt.blockNumber.toString();
+    transactionReceipt.gasUsed = transactionReceipt.gasUsed.toString();
+    transactionReceipt.cumulativeGasUsed =
+      transactionReceipt.cumulativeGasUsed.toString();
+    transactionReceipt.effectiveGasPrice =
+      transactionReceipt.effectiveGasPrice.toString();
+    return transactionReceipt;
+  }
+
+  async getServerWalletAddress() {
+    return await this.walletClient.getAddress();
+  }
+
+  mintTokens(address: any) {
+    return `Minting tokens for ${address}`;
+  }
+
+  checkMinterRole(address: string) {
+    throw new Error(`${address} Method not implemented.`);
   }
 }
